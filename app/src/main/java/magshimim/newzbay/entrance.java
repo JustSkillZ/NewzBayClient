d7a772c22176ff28a1286419e14eb6a775438286
Netanel <netanelm98@gmail.com>
2015-12-29 20:54:11 +0200

    UPDATED

--- app/src/main/java/magshimim/newzbay/entrance.java
+++ app/src/main/java/magshimim/newzbay/entrance.java
@@ -1,9 +1,12 @@
 package magshimim.newzbay;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,46 +29,51 @@ import com.facebook.Profile;
 import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 
-public class entrance extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
+public class entrance extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    public boolean is_facebook_log = false;
+    public boolean is_google_log = false;
     private Intent nfScreen = null;
-    private LoginButton loginButton;
+    private LoginButton facebook_loginButton;
+    private SignInButton google_signInButton;
     private CallbackManager callbackManager;
+    private GoogleApiClient mGoogleApiClient;
+    private GoogleSignInOptions gso;
+    private ProgressDialog mProgressDialog;
+    private static final int RC_SIGN_IN = 9001;
+    private static final String TAG = "check";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_entrance);
-        //startActivity(new Intent(this, SignInActivity.class));
-        if(Profile.getCurrentProfile() != null)
-        {
-            Intent nfScreen = new Intent(this, newsfeed.class);
-            startActivity(nfScreen);
+        facebook_login();
+        if (Profile.getCurrentProfile() != null) {
+            Log.d(TAG, "facebook login");
+            is_facebook_log = true;
+            movToNewsFeed();
         }
-        loginButton = (LoginButton)findViewById(R.id.btn_Facebook);
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                movToNewsFeed();
-            }
-
-            @Override
-            public void onCancel() {
-
-            }
-
-            @Override
-            public void onError(FacebookException e) {
-
-            }
-
-        });
+        else
+        {
+            is_facebook_log = false;
+            google_login();
+        }
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -82,8 +90,8 @@ public class entrance extends AppCompatActivity {
         AppEventsLogger.deactivateApp(this);
     }
 
-    public void signInAsGuest(View v)
-    {
+    public void signInAsGuest(View v) {
+        Log.d(TAG, "guest login");
         movToNewsFeed();
     }
 
@@ -91,5 +99,128 @@ public class entrance extends AppCompatActivity {
         Intent nfScreen = new Intent(this, newsfeed.class);
         startActivity(nfScreen);
         this.onStop();
+    }
+
+    public void facebook_login()
+    {
+        facebook_loginButton = (LoginButton) findViewById(R.id.btn_Facebook);
+        facebook_loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                is_facebook_log = true;
+                Log.d(TAG, "facebook login");
+                movToNewsFeed();
+            }
+
+            @Override
+            public void onCancel() {
+                is_facebook_log = false;
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                is_facebook_log = false;
+            }
+
+        });
+    }
+
+    public void google_login() {
+        findViewById(R.id.btn_Google).setOnClickListener(this);
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        google_signInButton = (SignInButton) findViewById(R.id.btn_Google);
+        google_signInButton.setSize(SignInButton.SIZE_STANDARD);
+        google_signInButton.setScopes(gso.getScopeArray());
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+            Log.d(TAG, "Got cached sign-in");
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            Log.d(TAG, "not cached");
+            showProgressDialog();
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(GoogleSignInResult googleSignInResult) {
+                    hideProgressDialog();
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("Loading");
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            Log.d(TAG, "google login");
+            is_google_log = true;
+            movToNewsFeed();
+        } else {
+            // Signed out, show unauthenticated UI.
+            is_google_log = false;
+        }
+    }
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
+
+    private void googleSignIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_Google:
+                googleSignIn();
+                break;
+
+        }
     }
 }

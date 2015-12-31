282480e77feb6eb92d01cf4def97f54928d740e0
Netanel <netanelm98@gmail.com>
2015-12-31 15:36:50 +0200

    UPDATED

--- app/src/main/java/magshimim/newzbay/newsfeed.java
+++ app/src/main/java/magshimim/newzbay/newsfeed.java
@@ -1,12 +1,23 @@
 package magshimim.newzbay;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Vector;
 import android.os.Handler;
+import android.widget.Toast;
 
 public class newsfeed extends AppCompatActivity {
 
@@ -23,6 +34,7 @@ public class newsfeed extends AppCompatActivity {
         toolbar_main = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar_main);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+        saveInInternalFolder("NewzBay", "check");
         Vector<Article> articles = new Vector<Article>();
         articles.add(new Article("Subject", "כותרת ראשית 1", "Second Headline", null, null, "Site", "http://www.ynet.co.il/articles/0,7340,L-4740637,00.html"));
         articles.add(new Article("Subject", "כותרת ראשית 2", "Second Headline", null, null, "Site", "http://www.google.co.il"));
@@ -41,6 +53,7 @@ public class newsfeed extends AppCompatActivity {
                 android.R.color.holo_orange_light,
                 android.R.color.holo_green_light,
                 android.R.color.holo_red_light);
+        Toast.makeText(this,("the string is: " + readInInternalFolder("check")), Toast.LENGTH_SHORT).show();
     }
 
     private final Runnable refreshListView = new Runnable(){
@@ -56,4 +69,42 @@ public class newsfeed extends AppCompatActivity {
             refreshList.setRefreshing(false);
         }
     };
+
+
+    private void saveInInternalFolder(String str1, String fileName)
+    {
+        FileOutputStream fos = null;
+        try
+        {
+            fos = openFileOutput(fileName, this.MODE_PRIVATE);
+            fos.write(str1.getBytes());
+            fos.close();
+        }
+        catch (IOException e)
+        {
+            Toast.makeText(this, "Error write internals", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+    private String readInInternalFolder(String fileName) {
+        Context context = getApplicationContext();
+
+
+        try {
+            FileInputStream fis = context.openFileInput(fileName);
+            InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+            BufferedReader bufferedReader = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            fis.close();
+            return (sb.toString());
+        } catch (IOException e) {
+            Toast.makeText(this, "Error read internals", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+            return "";
+        }
+    }
 }
